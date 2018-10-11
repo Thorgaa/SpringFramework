9ee40b2f39a9eeb46b7f78e1cbd436d3ecb4f398
Thorgaa <kozlowk22@wp.pl>
2018-10-11 21:26:27 +0200

    Wykład 19

--- spring5webapp/src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -13,7 +13,9 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
-    private String publisher;
+
+    @OneToOne
+    private Publisher publisher;
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
@@ -28,10 +30,9 @@ public class Book {
         this.authors = authors;
     }
 
-    public Book(String title, String isbn, String publisher) {
+    public Book(String title, String isbn) {
         this.title = title;
         this.isbn = isbn;
-        this.publisher = publisher;
     }
 
     public Long getId() {
@@ -58,13 +59,14 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public String getPublisher() {
+    public Publisher getPublisher() {
         return publisher;
     }
 
-    public void setPublisher(String publisher) {
+    public void setPublisher(Publisher publisher) {
         this.publisher = publisher;
     }
+
     public Set<Author> getAuthors() {
         return authors;
     }
---
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/model/Publisher.java
@@ -0,0 +1,63 @@
+package guru.springframework.spring5webapp.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String address;
+    private String city;
+    private String state;
+
+
+    public Publisher(String name, String address, String city, String state) {
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
