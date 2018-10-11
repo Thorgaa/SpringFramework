9d350af7a88e2761820cf6b6e99f9156df1f38f1
Thorgaa <kozlowk22@wp.pl>
2018-10-11 21:45:33 +0200

    Wykład 22

--- spring5webapp/src/main/java/guru/springframework/spring5webapp/model/Publisher.java
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/model/Publisher.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5webapp.model;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 public class Publisher {
@@ -59,5 +60,29 @@ public class Publisher {
 
     public void setState(String state) {
         this.state = state;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Publisher publisher = (Publisher) o;
+        return Objects.equals(id, publisher.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                '}';
     }
 }
